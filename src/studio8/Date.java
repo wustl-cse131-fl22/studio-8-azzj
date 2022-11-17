2326c013cd008d8c14a3743eb637c304e806805e
jiang <jiang@DESKTOP-KEL34FP.dhcp.wustl.edu>
2022-11-17 12:41:43 -0600

    Studio

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,78 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Date {
 
+	private int month;
+	private int day;
+	private int year;
+	private boolean holiday;
+	
+	public Date(int month, int day, int year, boolean holiday) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.holiday = holiday;
+	}
+	
+	public String toString() {
+		if(holiday == true) {
+			return "month: " + month  + "," + "day: " + day
+					+ "year: " + year + " " + "is holiday"; 
+		}
+		else {
+			return "month: " + month  + "," + "day: " + day
+					+ "year: " + year + " " + "is not holiday";
+		}
+		
+	}
 
-    public static void main(String[] args) {
+	
+	
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + day;
+		result = prime * result + month;
+		result = prime * result + year;
+		return result;
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		if (day != other.day)
+			return false;
+		if (month != other.month)
+			return false;
+		if (year != other.year)
+			return false;
+		return true;
+	}
+
+	public static void main(String[] args) {
+		List<Date> list = new LinkedList<>();
+		Date d1 = new Date(10,27,2022,false);
+		Date d2 = new Date(6,7,2001,true);
+		list.add(d1);
+		list.add(d2);
+		System.out.println(list);
+
+		
+		HashSet<Date> set = new HashSet<>();
+		set.add(d1);
+		set.add(d2);
+		System.out.println(set);
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -2,6 +2,15 @@ package studio8;
 
 
 public class Time {
+	private int hour;
+	private int minute;
+	private boolean format;
+	
+	public Time(int hour, int minute, boolean format) {
+		this.hour = hour;
+		this.minute = minute;
+		this.format = format;
+	}
 
 	public static void main(String[] args) {
  
